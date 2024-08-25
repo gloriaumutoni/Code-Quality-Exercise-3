65eec0197fb5c47d70080a6bec761e0a2745bba4
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-08-25 20:06:48 +0200

    checking if number is prime

--- primeNumbers.js
+++ primeNumbers.js
@@ -1,5 +1,6 @@
 const primeNumber = (number) => {
   if (number < 2) return false;
-  for (let i = 2; i < number; i++) if (number % i === 0) return false;
-  return true;
+  return typeof number === "number"
+    ? ![...Array(number).keys()].slice(2).some((i) => number % i === 0)
+    : false;
 };
