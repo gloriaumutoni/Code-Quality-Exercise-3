b3bbd6a9b988263bdd1b5850b947faa8287097f1
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-08-26 10:49:28 +0200

    implementing fibonnacci

--- exercise 4/fibonacci.js
+++ exercise 4/fibonacci.js
@@ -1,3 +1,7 @@
 // Implement a JavaScript function to calculate the Fibonacci sequence up to a given number.
-const fibonacci = (number) =>
-  number > 1 ? number * fibonacci(number - 1) : number;
+
+const fibonacci=(n)=>  Array.from({ length: n }).reduce((accumulator, _, i) => {
+      if (i === 0) return [0];
+      if (i === 1) return [0, 1];
+      return [...accumulator, accumulator[i - 1] + accumulator[i - 2]];
+  }, []);
